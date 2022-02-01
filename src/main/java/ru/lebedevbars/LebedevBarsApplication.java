58682fbcf31ab26471b9c0333c245488253b72f6
l6b6d6v <l6b6d6v@gmail.com>
2022-02-01 13:46:17 +0400

    Initial commit

---
+++ src/main/java/ru/lebedevbars/LebedevBarsApplication.java
@@ -0,0 +1,13 @@
+package ru.lebedevbars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LebedevBarsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LebedevBarsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/lebedevbars/LebedevBarsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.lebedevbars;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LebedevBarsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
